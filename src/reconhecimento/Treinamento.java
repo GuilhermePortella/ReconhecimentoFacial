9c1ef5b397cef622ed504578ad65153ee782c53f
Guilherme Portella <guilhermeportella2@gmail.com>
2021-06-04 12:19:37 -0300

    Commit EigenFace

---
+++ src/reconhecimento/Treinamento.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reconhecimento;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.nio.IntBuffer;
+import org.bytedeco.javacpp.opencv_core;
+import static org.bytedeco.javacpp.opencv_core.CV_32SC1;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.MatVector;
+import static org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Treinamento {
+    public static void main(String[] args) {
+        File diretorio = new File("src\\fotos");
+        FilenameFilter filtroImagem = new FilenameFilter(){
+            @Override
+            public boolean accept(File dir, String nome){
+                return nome.endsWith(".jpg") || nome.endsWith(".gif")|| nome.endsWith(".png");
+            }
+        };
+        
+        File[] arquivos = diretorio.listFiles(filtroImagem);
+        MatVector fotos = new MatVector(arquivos.length);
+        Mat rotulos = new Mat(arquivos.length, 1, CV_32SC1);
+        IntBuffer rotulosBuffer = rotulos.createBuffer();
+        int contador = 0;
+        for (File imagem: arquivos) {
+            Mat foto = imread(imagem.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);
+            int classe = Integer.parseInt(imagem.getName().split("\\.")[1]);
+            System.out.println(classe);
+            
+        }
+    }
+    
+}
