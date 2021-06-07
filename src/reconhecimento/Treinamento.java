608ddcf1fe1a83349c4d02df9510de88a1d67a16
Guilherme Portella <guilhermeportella2@gmail.com>
2021-06-07 16:07:26 -0300

    Commit Reconhecimento Facial

--- src/reconhecimento/Treinamento.java
+++ src/reconhecimento/Treinamento.java
@@ -12,34 +12,55 @@ import org.bytedeco.javacpp.opencv_core;
 import static org.bytedeco.javacpp.opencv_core.CV_32SC1;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.MatVector;
+import org.bytedeco.javacpp.opencv_core.Size;
+import org.bytedeco.javacpp.opencv_face;
+import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
+import static org.bytedeco.javacpp.opencv_face.createEigenFaceRecognizer;
+import static org.bytedeco.javacpp.opencv_face.createFisherFaceRecognizer;
+import static org.bytedeco.javacpp.opencv_face.createLBPHFaceRecognizer;
 import static org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+import static org.bytedeco.javacpp.opencv_imgproc.resize;
 
 /**
  *
  * @author Guilherme
  */
 public class Treinamento {
+
     public static void main(String[] args) {
         File diretorio = new File("src\\fotos");
-        FilenameFilter filtroImagem = new FilenameFilter(){
+        FilenameFilter filtroImagem = new FilenameFilter() {
             @Override
-            public boolean accept(File dir, String nome){
-                return nome.endsWith(".jpg") || nome.endsWith(".gif")|| nome.endsWith(".png");
+            public boolean accept(File dir, String nome) {
+                return nome.endsWith(".jpg") || nome.endsWith(".gif") || nome.endsWith(".png");
             }
         };
-        
+
         File[] arquivos = diretorio.listFiles(filtroImagem);
         MatVector fotos = new MatVector(arquivos.length);
         Mat rotulos = new Mat(arquivos.length, 1, CV_32SC1);
         IntBuffer rotulosBuffer = rotulos.createBuffer();
         int contador = 0;
-        for (File imagem: arquivos) {
+        for (File imagem : arquivos) {
             Mat foto = imread(imagem.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);
             int classe = Integer.parseInt(imagem.getName().split("\\.")[1]);
             System.out.println(classe);
-            
+            resize(foto, foto, new Size(160, 160));
+            fotos.put(contador, foto);
+            rotulosBuffer.put(contador, classe);
+            contador++;
         }
+        FaceRecognizer eigenfaces = createEigenFaceRecognizer();
+        FaceRecognizer fisherfaces = createFisherFaceRecognizer();
+        FaceRecognizer lbph = createLBPHFaceRecognizer();
+        
+        eigenfaces.train(fotos, rotulos);
+        eigenfaces.save("src\\recursos\\classificadorEigenFaces.yml");
+        fisherfaces.train(fotos, rotulos);
+        fisherfaces.save("src\\recursos\\classificadorFisherFaces.yml");
+        lbph.train(fotos, rotulos);
+        lbph.save("src\\recursos\\classificadorLBPH.yml");
     }
-    
+
 }
